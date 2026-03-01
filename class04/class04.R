60e9e4a8f14467feeb7ea567a37db313dabc911d
poojabio <poojapp7a@gmail.com>
2026-03-01 07:45:11 -0800

    Copying all class work so far (classes 4 to 14)

---
+++ class04/class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Class 4 - Intro to R - BIMM 143"
+#' author: "Pooja Parthasarathy"
+#' date: "January 16th, 2026"
+#' ---
+
+x <- 1:50
+#plot(x)
+plot(x, sin(x), typ="l", col = "blue", lwd=3, xlab = "X LABEL GOES HERE")
